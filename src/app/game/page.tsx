6cce798556351525dff54c9e0d4363930c4954f1
root <savvascyp@hotmail.com>
2024-04-02 12:23:11 +0000

    Display ads

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -98,6 +98,20 @@ export default function GamePage() {
     {
       setGameStateToLocalStorage()
     }
+
+      <div dangerouslySetInnerHTML={{__html: `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2194018182042050"
+          crossorigin="anonymous"></script>
+      <!-- Pou en touton -->
+      <ins class="adsbygoogle"
+          style="display:block"
+          data-ad-client="ca-pub-2194018182042050"
+          data-ad-slot="3596258713"
+          data-ad-format="auto"
+          data-full-width-responsive="true"></ins>
+      <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>`}}></div>
+      
       <header>
         <img src={`${BASE_PATH}/pouentouto-logo.png`} alt="" />
       </header>
